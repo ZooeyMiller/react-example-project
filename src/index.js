83f7e381f9da60b0dff160a59d586bd5bebc6218
zooeymiller <zooeyxmiller@gmail.com>
2018-05-15 16:43:58 +0100

    add drake toggling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React, { Fragment, Component } from 'react';
-import { render } from 'react-dom';
-import drakes from './drakes';
+import React, { Fragment, Component } from "react";
+import { render } from "react-dom";
+import drakes from "./drakes";
 
 const randomWithin = within => Math.floor(Math.random() * within) + 1;
 
-const Drake = ({ name, src, x, y, className = '' }) => (
+const Drake = ({ name, src, x, y, className = "" }) => (
   <img
     src={src}
     alt={name}
@@ -17,35 +17,59 @@ const Drake = ({ name, src, x, y, className = '' }) => (
 );
 
 const getAnimationClass = index =>
-  'drake-' +
-  (index % 3 === 0 ? 'wiggle' : index % 2 === 0 ? 'wobble' : 'v-wiggle');
+  "drake-" +
+  (index % 3 === 0 ? "wiggle" : index % 2 === 0 ? "wobble" : "v-wiggle");
 
 const Drakes = ({ drakes }) =>
   drakes.reduce(
-    (acc, { name, src, x, y }, i) => [
-      ...acc,
-      <Drake
-        src={src}
-        name={name}
-        x={x}
-        y={y}
-        key={name + x}
-        className={getAnimationClass(i)}
-      />,
-      <Drake
-        src={src}
-        name={name}
-        x={y - 75}
-        y={x}
-        key={name + y}
-        className={getAnimationClass(i + 1)}
-      />
-    ],
+    (acc, { name, src, x, y, display }, i) =>
+      display
+        ? [
+            ...acc,
+            <Drake
+              src={src}
+              name={name}
+              x={x}
+              y={y}
+              key={name + x}
+              className={getAnimationClass(i)}
+            />,
+            <Drake
+              src={src}
+              name={name}
+              x={y - 75}
+              y={x}
+              key={name + y}
+              className={getAnimationClass(i + 1)}
+            />
+          ]
+        : acc,
     []
   );
 
+const DrakeButtons = ({ drakes, onClick }) =>
+  drakes.map(drake => <button onClick={onClick(drake)}>{drake.name}</button>);
+
+const findDrakeIndex = (drakes, { name: nameToFind }) =>
+  drakes.findIndex(({ name }) => name === nameToFind);
+
+const toggleDrakeDisplay = (drakes, drakeToToggle) => {
+  const index = findDrakeIndex(drakes, drakeToToggle);
+  return [
+    ...drakes.slice(0, index),
+    { ...drakeToToggle, display: !drakeToToggle.display },
+    ...drakes.slice(index + 1)
+  ];
+};
+
 class App extends Component {
   state = { drakes, height: 600, width: 600 };
+
+  handleDrakeClick = drake => () => {
+    this.setState({
+      drakes: toggleDrakeDisplay(this.state.drakes, drake)
+    });
+  };
 
   render() {
     return (
@@ -54,9 +78,13 @@ class App extends Component {
         <div className="drake-container">
           <Drakes drakes={this.state.drakes} />
         </div>
+        <DrakeButtons
+          drakes={this.state.drakes}
+          onClick={this.handleDrakeClick}
+        />
       </Fragment>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
